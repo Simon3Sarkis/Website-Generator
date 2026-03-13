4db77b5f78dedc52c81e0695150c5489a8a5e455
Simon3Sarkis <simon.sarkis3@gmail.com>
2026-03-13 19:53:07 -0400

    Added block markdown

---
+++ src/block_markdown.py
@@ -0,0 +1,12 @@
+def markdown_to_blocks(markdown):
+
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+
+    for block in blocks:
+        block = block.strip()
+        
+        if block != "":
+            filtered_blocks.append(block)
+            
+    return filtered_blocks
